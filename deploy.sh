64d6b740d7685bbe52afde9c96b3e71f762f1487
Gavin Zhou <gavin.zhou@gmail.com>
2017-01-31 21:43:24 +0900

    fix gcr url

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ if [ -z "${_tag}" ]; then
     _tag=${_VERSION}
 fi
 
-docker tag "orangesys/alpine-kubebot:${_tag}" "asia.gcr.io/orange-sys/alpine-kubebot:${_tag}"
+docker tag "orangesys/alpine-kubebot:${_tag}" "asia.gcr.io/saas-orangesys-io/alpine-kubebot:${_tag}"
 docker login -e $DOCKER_EMAIL -u _json_key -p "$(cat ${HOME}/account-auth.json)" https://asia.gcr.io
-docker push asia.gcr.io/orange-sys/alpine-kubebot:${_tag}
+docker push asia.gcr.io/saas-orangesys-io/alpine-kubebot:${_tag}
